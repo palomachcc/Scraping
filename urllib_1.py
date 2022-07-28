61c81a5e4ca7539b34337c5e1dfd58c7323e1f3a
palomachcc <110131341+palomachcc@users.noreply.github.com>
2022-07-28 15:19:40 -0300

    Delete urllib_1.py

--- urllib_1.py
+++
@@ -1,9 +0,0 @@
-import urllib.request    #importo la libreria. Esta tiene 4 modulos, el que uso aca es el .request
-
-
-page=input("Enter de page: ")
-fhand = urllib.request.urlopen(page) # http://data.pr4e.org/romeo.txt
-for line in fhand:
-    print(line.decode().strip())
-
-exit=input("\nPulse enter para salir")
