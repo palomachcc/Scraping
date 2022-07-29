68278f22d21b6ec39eedeb179c0d3d42ae543807
palomachcc <110131341+palomachcc@users.noreply.github.com>
2022-07-29 20:40:22 -0300

    Add files via upload
    mismo resultado pero no usa decode() sino que usa directamente la funcion de BS

---
+++ scraping_1.c.py
@@ -0,0 +1,15 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url=input("enter URL: ") # http://www.dr-chuck.com/page1.htm
+html=urllib.request.urlopen(url).read()   #html me devuelve un UTF-8 string
+soup=BeautifulSoup(html,"html.parser")    #con BS hace "limpieza", le entregas la cosa "html" y nos devuelve un objeto limpio
+
+print(soup)
+
+#recupero todos los links
+#tags=soup("a")
+#for tag in tags:
+#    print(tag.get("href",None))
+
+exit=input("Presione enter para salir")
