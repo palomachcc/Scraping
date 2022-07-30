de0242fe4b86d33740ba533310cd0c1dfa42eff8
palomachcc <110131341+palomachcc@users.noreply.github.com>
2022-07-30 13:29:47 -0300

    Update scraping_2.py

--- scraping_2.py
+++ scraping_2.py
@@ -1,8 +1,8 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import ssl
 
+#hay una parte de errores que no aprendi todavia.
 
 count=0
 url = input('Enter page:  ') # http://py4e-data.dr-chuck.net/comments_1561778.html
@@ -10,14 +10,16 @@ html = urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-tags = soup('span')
+tags = soup('span')           #tags es una lista de : lo que le pida, en este caso el ejercicios me indicaba:You are to find all the <span> tags in the file and pull out the numbers from the tag and sum the numbers.
 for tag in tags:
     # Look at the parts of a tag
     #print('TAG:', tag)
     #print('URL:', tag.get('href', None))
-    #print('Contents:', tag.contents[0])
+    #print('Contents:', tag.contents[0])   me devuelve el contenido, osea la cantidad de comentarios, para este caso que pedi --soup("span")--.
     #print('Attrs:', tag.attrs)
     count=count + int(tag.contents[0])
-
+    
+#print(tags) fijate que si imprimis eso, es una lista. https://www.w3schools.com/python/python_lists.asp    
+    
 print(count)
 exit=input("Press enter to exit")
